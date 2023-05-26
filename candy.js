abf4a6424e421c691e2c5933d5834d6fa65686ec
Mutu9820 <126443399+Mutu9820@users.noreply.github.com>
2023-05-26 19:10:39 +0800

    Update candy.js

--- candy.js
+++ candy.js
@@ -1,21 +1,101 @@
-let all = 0;
-let money = 0;
-function toots(){
-    let candy = document.getElementById("candy").value;
-    let too = document.getElementById("too").value;
-    if (candy="sneakers"){
-            money = too * 2500;
-            all = all + money;
-        document.getElementById("hariu").innerHTML = all;
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'Kontik',
+        price: 2000
+    },
+    {
+        id: 2,
+        name: 'Oreo',
+        price: 2500
+    },
+    {
+        id: 3,
+        name: 'Twix',
+        price: 2000
+    },
+    {
+        id: 4,
+        name: 'Cofler Block',
+        price: 3500
+    },
+    {
+        id: 5,
+        name: 'Mars',
+        price: 3000
+    },
+    {
+        id: 6,
+        name: 'merci',
+        price: 5000
     }
-    if (candy="bonobon"){
-            money = too * 3000;
-            all = all + money;
-        document.getElementById("hariu").innerHTML = all;
+];
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Add To Card</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCard(key){
+    if(listCards[key] == null){
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
     }
-    if (candy = "alilabala") {
-            money = too * 1500;
-            all = all + money;
-         document.getElementById("hariu").innerHTML = all;
+    reloadCard();
+}
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="image/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
     }
+    reloadCard();
 }
